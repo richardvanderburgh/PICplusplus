fa296d7c0b3af2db491b5727a01f5289050808ad
Richard Vanderburgh <vanderburghrichie@gmail.com>
2023-05-31 23:32:20 -0400

    Reorganized funcitions into header files

--- lib/include/init.hpp
+++ lib/include/init.hpp
@@ -1,35 +1,48 @@
 #pragma once
 
+#include <boost/algorithm/algorithm.hpp>
+#include <fft.hpp>
+#include <fftw3.h>
+#include <matplot/matplot.h>
+#include <nlohmann/json.hpp>
+
 #include <algorithm>
 #include <array>
 #include <cmath>
 #include <corecrt_math_defines.h>
 #include <iostream>
-#include <matplot/matplot.h>
 #include <thread>
 #include <vector>
 
-#include "fft.hpp"
-#include <boost/algorithm/algorithm.hpp>
-
-#include <fftw3.h>
-#include <nlohmann/json.hpp>
-
+#include "Accel.hpp"
+#include "Fields.hpp"
+#include "SetRho.hpp"
 
 class Init {
 public:
 
-	
-	struct FrameData {
+	struct SpeciesData {
 
-		std::vector<std::vector<double>> xData;
-		std::vector<std::vector<double>> vxData;
+		int speciesIndex;
+		std::vector<double> positions;
+		std::vector<double> velocities;
+	};
+
+	//struct ParticalData {
+	//	double position;
+	//	double velocity;
+	//	std::string species;
+	//	int id;
+	//};
+
+	struct FrameData {
+		//std::vector <ParticalData> speciesData;
+		std::vector <SpeciesData> speciesData;
+
 	};
 
 	struct PicData {
-
 		std::vector<FrameData> frameData;
-
 	};
 
 	void initialize(int N1, int nt) {
@@ -295,7 +308,6 @@ public:
 				}
 			}
 
-			// From ES1.f
 			// Add random Maxwellian.
 			if (vt1[species] != 0) {
 				for (int I = 0; I < N[species]; ++I) {
@@ -343,67 +355,16 @@ public:
 		std::vector<std::vector<double>> qjp1data(nsp, std::vector<double>(ng + 1, 0.0));
 		std::vector<std::vector<double>> drhodata(nsp, std::vector<double>(ng + 1, 0.0));
 
-		double dxi;
-		int xn;
-
 		for (int species = 0; species < nsp; species++) {
-			// If it is the first group of particles, then clear out rho.
-			if (species == 0) {
-				for (int j = 1; j < ng + 1; j++) {
-					rho[j] = rho0[1][j];
-				}
-				rho[1] = 0;
-				xn = ng;
-				dxi = 1.0 / dx;
-			}
-			// Add on fixed neutralizing charge density
-			// (not needed when all species are mobile - but harmless.)
-			for (int j = 0; j < rhos[0].size(); j++) {
-				rhos[species][j] = 0;
-			}
-			for (int j = 1; j <= ng; j++) {
-				rho0[species][j] = rho0[species][j] - rhos[species][j];
-				rho[j] = rho[j] - rhos[species][j];
-			}
 
-			// NGP
-			if (iw == 1) {
-				for (int i = 1; i <= N[species]; i++) {
-					x[species][i] = x[species][i] * dxi;
-					if (x[species][i] < 0) {
-						x[species][i] = x[species][i] + xn;
-					}
-					if (x[species][i] > xn) {
-						x[species][i] = x[species][i] - xn;
-					}
-					int j = floor((x[species][i]) + 1 + 0.5);
-					rho[j] = rho[j] + qdx[species];
-				}
-			}
-			// Linear
-			else if (iw == 2) {
-				for (int i = 0; i < N[species]; i++) {
-					x[species][i] = x[species][i] * dxi;
-					if (x[species][i] < 0) {
-						x[species][i] = x[species][i] + xn;
-					}
-					if (x[species][i] > xn) {
-						x[species][i] = x[species][i] - xn;
-					}
-					int j = floor(x[species][i]);
-					//jdata(i, species) = j;
-					double drho = qdx[species] * (x[species][i] - j);
-					//drhodata(i, species) = drho;
-					rho[j] = rho[j] - drho + qdx[species];
-					rho[j + 1] = rho[j + 1] + drho;
-				}
-			}
-			// SETV
+			retRho(species, ng, dx, N, qdx, rho, x, rho0, rhos, iw);
+			// Velocities
 			for (int K = 0; K < N[species]; ++K) {
 				vx[species][K] *= dtdx;
 			}
 		}
 
+		// Acceleration
 		std::vector<double> a(ng + 1);
 
 		int t = 0;
@@ -415,7 +376,7 @@ public:
 		// Create a figure and set the x and y limits
 		//matplot::figure("Animation");
 
-		PicData picData;
+		//PicData picData;
 		nlohmann::json frames;
 
 		for (int t = 1; t <= nt; t++) {
@@ -425,16 +386,44 @@ public:
 			fields(rho, L, iw, dx, E, t, ng, a, ael);
 
 			FrameData frameData;
-			frameData.xData = x;
-			frameData.vxData = vx;
+
+			// Store the data for the current frame (for all species)
+			for (int sp = 0; sp < nsp; sp++) {
+				SpeciesData speciesData;
+				speciesData.speciesIndex = sp;
+				speciesData.positions = x[sp];
+				speciesData.velocities = vx[sp];
+				frameData.speciesData.push_back(speciesData);
+			}
+
+			nlohmann::json speciesArray;
+			for (const auto& species : frameData.speciesData) {
+				nlohmann::json speciesObject;
+				speciesObject["speciesIndex"] = species.speciesIndex;
+				speciesObject["positions"] = species.positions;
+				speciesObject["velocities"] = species.velocities;
+				speciesArray.push_back(speciesObject);
+			}
 
 			nlohmann::json frame;
-			frame["positions"] = x[0];
-			frame["velocities"] = vx[0];
+			//nlohmann::json particle;
+			for (const auto& species : frameData.speciesData) {
+				nlohmann::json speciesObject;
+				speciesObject["speciesIndex"] = species.speciesIndex;
+				speciesObject["positions"] = species.positions;
+				speciesObject["velocities"] = species.velocities;
+				speciesArray.push_back(speciesObject);
+			}
+
+			frame["timeStep"] = t;  // Store the time step index
+			frame["speciesData"] = speciesArray;
+
+			//nlohmann::json frame;
+			//frame["positions"] = x[0];
+			//frame["velocities"] = vx[0];
 			frames.push_back(frame);
 
-			picData.frameData.push_back(frameData);
-
+			//picData.frameData.push_back(frameData);
 
 			//// Create a scatter plot 
 			//matplot::scatter(x[0], vx[0], 1);
@@ -445,160 +434,9 @@ public:
 			//matplot::hold(false);
 			//std::cout << "t = " << t << std::endl;
 		}
-			std::cout << frames.dump() << std::endl;
+		std::cout << frames.dump() << std::endl;
 	}
-	void fields(std::vector<double>& rho, double L, int iw, double dx, std::vector<std::vector<double>>& E, int t, const int constNg, std::vector<double>& a, double ael)
-	{
-		// FIELDS - E field solver
-		const int ng = 32;
-		std::vector<double> phi(ng + 1, 0.0);
-		double l = L;
-
-		// Transform charge density.
-		rho[0] += rho[ng];
-		rho[ng] = rho[0];
-
-		complex complexRho[ng];
-		complex complexRhok[ng];
-		complex complexPhik[ng];
-
-		for (int i = 0; i < ng; i++) { complexRho[i] = rho[i]; }
-		CFFT::Forward(complexRho, complexRhok, ng);
-
-		double tol = 1;
-		for (int k = 0; k < ng; k++) {
-			int ii;
-			if (k == 0) {
-				ii = tol;
-			}
-			else if (k <= ng / 2 && k >= 0) {
-				ii = k;
-			}
-			else if (k > ng / 2) {
-				ii = k - ng;
-			}
-
-			complexPhik[k] = complexRhok[k] / -std::pow(2.0 * M_PI * ii / L, 2.0);
-			//esestot[t + 1] = (esestot[t + 1] - (phik[k] * rhok[k])) / 2;
-		}
-
-		complex complexPhi[ng];
-		CFFT::Inverse(complexPhik, complexPhi, ng);
-
-		// Perform FFT using FFTW
-		std::vector<std::complex<double>> output(ng);
-		fftw_plan plan = fftw_plan_dft_r2c_1d(ng, rho.data(), reinterpret_cast<fftw_complex*>(output.data()), FFTW_ESTIMATE);
-		fftw_execute(plan);
-		fftw_destroy_plan(plan);
-
-
-		for (int i = 0; i < ng; i++) {
-			phi[i] = -complexPhi[i].re();
-		}
-
-		phi[ng] = phi[0];
-
-		//Update esem using phi and rhok
-	   //for (int k = 0; k < mplot + 1; k++) {
-	   //	esem(t + 1, k) -= phik(k) * rhok(k) / 2.0;
-	   //}
-		if (iw == 1 || iw == 2)
-		{
-			for (int j = 1; j < ng; j++) {
-				E[j][t] = (phi[j - 1] - phi[j + 1]) / (2.0 * dx);
-			}
-			E[0][t] = (phi[ng - 1] - phi[1]) / (2.0 * dx);
-			E[ng][t] = E[0][t];
-		}
-		else if (iw == 3)
-		{
-			double dxi = 1.0 / dx;
-			for (int j = 0; j < ng; j++) {
-				E[j][t] = (phi[j] - phi[j + 1]) * dxi;
-			}
-			E[ng][t] = E[0][t];
-		}
-
-		ael = 1;
-		for (int i = 0; i <= ng; i++) {
-			a[i] = E[i][t];
-		}
-	}
-
-	void accel(int nsp, double dx, double dt, int t, std::vector<double> q, std::vector<double> m, double& ael, std::vector<double>& a, int ng, std::vector<int> N, std::vector <std::vector<double>>& x, std::vector <std::vector<double>>& vx) {
-		/// ACCEL - Calculate force and advance velocity
-
-		for (int species = 0; species < nsp; species++) {
-
-			if (t == 0)
-				q[species] = -0.5 * q[species];
-
-			double dxdt = dx / dt;
-			double ae = (q[species] / m[species]) * (dt / dxdt);
-
-			//  renormalizes acceleration if need be.
-
-			if (ae != ael) {
-				double tem = ae / ael;
-				for (int j = 0; j < ng + 1; j++) {
-					a[j] *= tem;
-				}
-				ael = ae;
-			}
-
-			double v1s = 0;
-			double v2s = 0;
-			for (int i = 0; i < N[species]; ++i) {
-				int j = floor(x[species][i]);
-				double vo = vx[species][i];
-				double vn = vo + a[j] + (x[species][i] - j - 1) * (a[j + 1] - a[j]);
-				v1s += vn;
-				v2s += vo * vn;
-				vx[species][i] = vn;
-			}
-		}
-	}
-
-	void move(int nsp, std::vector<double>& rho, std::vector<std::vector<double>> rho0, std::vector<double> qdx, std::vector<int> N, std::vector <std::vector<double>>& x, std::vector <std::vector<double>>& vx, int ng) {
-		//MOVE - Advances position one time step and accumulates charge density.
-		for (int species = 0; species < nsp; species++) {
-			// Clear out old charge density.
-			for (int j = 1; j <= ng; j++) {
-				rho[j] = rho0[species][j];
-			}
-			rho[0] = 0;
-		}
-
-		for (int species = 0; species < nsp; species++) {
-			for (int i = 0; i < N[species]; i++) {
-
-				x[species][i] += vx[species][i];
-
-				if (x[species][i] < 0)
-					x[species][i] += ng;
-
-				if (x[species][i] >= ng)
-					x[species][i] -= ng;
-
-
-				int j = floor(x[species][i]);
-				double drho = qdx[species] * (x[species][i] - j);
-				rho[j] = rho[j] - drho + qdx[species];
-				rho[j + 1] = rho[j + 1] + drho;
-			}
-		}
-
-	}
-	//void retRho() {
-	//	double qdx = q / dx;
-	//	std::vector<double> rho(ng + 1, 0.0); // charge density at the spatial grid points
-	//	std::vector<std::vector<double>> rhos(ng + 1, std::vector<double>(nsp, 0.0));
-	//	std::vector<std::vector<double>> rho0(ng + 1, std::vector<double>(nsp, 0.0));
-	//	std::vector<std::vector<double>> qjdata(maxN, std::vector<double>(nsp, 0.0));
-	//	std::vector<std::vector<double>> qjp1data(maxN, std::vector<double>(nsp, 0.0));
-	//	std::vector<std::vector<double>> drhodata(maxN, std::vector<double>(nsp, 0.0));
-	//}
-
+	
 	std::vector<double> linspace(double start, double end, int num_points) {
 		std::vector<double> result(num_points);
 		double step = (end - start) / (num_points - 1);
